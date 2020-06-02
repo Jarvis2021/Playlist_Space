12f71586596766361b37156075f9c3f462ac2a98
Connor <connorskorburg@gmail.com>
2020-06-01 20:30:30 -0500

    added backend functionality

--- playlistofchoice/urls.py
+++ playlistofchoice/urls.py
@@ -5,8 +5,17 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('sign_in', views.sign_in),
+    path('callback', views.callback),
     path('genremoods', views.genre_mood),
     path('newrelease', views.newreleases),
     path('chart', views.charts),
     path('concert', views.concerts),
+    path('search_track', views.search_track),
+    path('track_results', views.track_results),
+    path('get_playlists', views.get_playlists),
+    path('new_playlist', views.new_playlist),
+    path('create_playlist', views.create_playlist),
+    path('add_song_to_playlist/<str:track_id>', views.add_song_to_playlist),
+    path('new_song_in_playlist', views.new_song_in_playlist),
 ]
